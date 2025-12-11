b8e63493f3b20b203201494b1f5028c7429a8a06
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2025-12-11 15:20:27 +0000

    feat: Add factory functions for task and project.

---
+++ src/factories.js
@@ -0,0 +1,56 @@
+function createTask (title, description, dueDate, priority, notes, checklist) {
+    const taskTitle = title;
+    const taskDescription = description;
+    const taskDueDate = dueDate;
+    const taskPriority = priority;
+    const taskNotes = notes;
+    const taskChecklist = checklist;
+
+    const getTitle = () => taskTitle;
+    const setTitle = (newTitle) => this.taskTitle = newTitle;
+
+    const getDescription = () => taskDescription;
+    const setDescription = (newDescription) => this.taskDescription = newDescription;
+
+    const getDueDate = () => taskDueDate;
+    const setDueDate = (newDueDate) => this.taskDueDate = newDueDate;
+
+    const getPriority= () => taskPriority;
+    const setPriority = (newPriority) => this.taskPriority = newPriority;
+
+    const getNotes = () => taskNotes;
+    const setNotes = (newNotes) => this.taskNotes = newNotes;
+
+    const getChecklist = () => taskChecklist;
+    const setChecklist = (newChecklist) => this.taskChecklist = newChecklist;
+
+    return {getTitle, setTitle, 
+            getDescription, setDescription, 
+            getDueDate, setDueDate, 
+            getPriority, setPriority, 
+            getNotes, setNotes, 
+            getChecklist, setChecklist
+        };
+}
+
+function createProject (title, description, tasks) {
+    const projectTitle = title;
+    const projectDescription = description;
+    const projectTasks = tasks;
+
+    const getTitle = () => projectTitle;
+    const setTitle = (newTitle) => this.projectTitle = newTitle;
+
+    const getDescription = () => projectDescription;
+    const setDescription = (newDescription) => this.projectDescription = newDescription;
+
+    const getTasks = () => projectTasks;
+    const setTasks = (newTasks) => this.projectTasks = newTasks;
+
+    return {getTitle, setTitle, 
+            getDescription, setDescription, 
+            getTasks, setTasks
+    };
+}
+
+export {createTask, createProject};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import "./styles.css";
+import { createTask, createProject } from "./factories";
